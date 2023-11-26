aaa9afd20af69841fe6096b6f137ff681b38e791
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2023-11-25 18:59:38 -0600

    Utilize new Gemwork dependencies + test support
    This simplifies definition and update of gem dependencies and
    utilization of test/test_helper.rb support/requires.

--- object_identifier.gemspec
+++ object_identifier.gemspec
@@ -40,13 +40,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "byebug"
   spec.add_development_dependency "gemwork"
-  spec.add_development_dependency "minitest"
-  spec.add_development_dependency "minitest-reporters"
-  spec.add_development_dependency "much-stub"
   spec.add_development_dependency "pry"
   spec.add_development_dependency "pry-byebug"
   spec.add_development_dependency "rake"
-  spec.add_development_dependency "reek"
-  spec.add_development_dependency "simplecov"
   spec.add_development_dependency "solargraph"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,32 +1,12 @@
 # frozen_string_literal: true
 
-require "simplecov"
-SimpleCov.start do
-  add_filter "/bin/"
-  add_filter "/test/"
-end
-puts "SimpleCov enabled."
+require "gemwork/test/support/simplecov"
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "object_identifier"
 
 require "minitest/autorun"
-require "minitest/reporters"
-require "pry"
 
-require "much-stub"
-
-Minitest::Test.make_my_diffs_pretty!
-reporter_options = { color: true }
-Minitest::Reporters.use!(
-  Minitest::Reporters::DefaultReporter.new(reporter_options))
-
-def context(...)
-  describe(...)
-end
-
-class Minitest::Spec
-  after do
-    MuchStub.unstub!
-  end
-end
+require "gemwork/test/support/reporters"
+require "gemwork/test/support/much_stub"
+require "gemwork/test/support/spec_dsl"
